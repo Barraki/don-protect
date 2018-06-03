6845eed674b9b2c71150771aa079655d0fa444a6
IGor <33422772+Barraki@users.noreply.github.com>
2018-06-04 02:31:13 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,5 +1,64 @@
-$(function() {
+$(function () {
 
-	// Custom JS
+	$(window).ready(function () {
+		var width = $('.slider-img').width()
+		var height = $('.change_block').height()
 
+		$('.slider-wrap>img').width(width);
+		$('.slider-wrap').width(width * $('.slider-wrap>img').length);
+
+		$('.slider-wrap').css('left', -width);
+		$('.slider-wrap>img:last-child').prependTo('.slider-wrap');
+
+		function nextSlide() {
+			$('.slider-wrap').animate({
+				'margin-left': -width
+			}, 500, function () {
+				$('.slider-wrap>img:first-child').appendTo('.slider-wrap');
+				$('.slider-wrap').css('margin-left', 0);
+			});
+
+			$('.change_block').animate({}, 300, function () {
+				$('.change_block>div:first-child').appendTo('.change_block');
+			});
+
+
+		}
+
+		function prevSlide() {
+			$('.slider-wrap').animate({
+				'margin-left': width
+			}, 500, function () {
+				$('.slider-wrap>img:last-child').prependTo('.slider-wrap');
+				$('.slider-wrap').css('margin-left', 0);
+			});
+		}
+
+		$('.nextT').click(nextSlide)
+		$('.prevV').click(prevSlide)
+
+
+		$(document).scroll(function () {
+			if ($(this).scrollTop() > 1) {
+				$('.nav').addClass("nav-fixed");
+				$('.logo').removeClass("col-2");
+				$('.logo').addClass("col-3");
+				$('.img_logo').addClass("logo-fixed");
+				$('.logo_text').css('display', 'block');
+			} else {
+				$('.nav').removeClass("nav-fixed");
+				$('.img_logo').removeClass("logo-fixed");
+				$('.logo').removeClass("logo-fixed");
+				$('.logo').removeClass("col-3");
+				$('.logo').addClass("col-2");
+				$('.logo_text').css('display', 'none');
+			}
+		});
+
+		$("#slider1").tinycarousel({
+			axis: "y",
+			animationTime: 450
+		});
+
+	})
 });
